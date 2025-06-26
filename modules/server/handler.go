9ee7611dfac61814894146064afccefcaad22dfc
XzerozZ <111548296+XzerozZ@users.noreply.github.com>
2025-06-26 10:10:13 +0700

    Cors new configs

--- modules/server/handler.go
+++ modules/server/handler.go
@@ -21,8 +21,8 @@ func SetupRoutes(app *fiber.App, jwt configs.JWT, supa configs.Supabase, mail co
 	}
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://beside-mom.vercel.app, https://www.besidemom.com , http://localhost:3000",
-		AllowMethods: "GET, POST, PUT, DELETE",
+		AllowOrigins: "https://beside-mom.vercel.app, https://www.besidemom.com",
+		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}))
 
